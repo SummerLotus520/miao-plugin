00e8f4b38a06e7ed08c577748a9388fac395dea2
SummerLotus520 <2402804043@qq.com>
2026-03-25 19:08:37 +0800

    fix

--- models/dmg/DmgAttr.js
+++ models/dmg/DmgAttr.js
@@ -1,6 +1,3 @@
-/*
-* 伤害计算 - 属性计算
-* */
 import { eleBaseDmg } from './DmgCalcMeta.js'
 import lodash from 'lodash'
 import DmgMastery from './DmgMastery.js'
@@ -12,51 +9,45 @@ let DmgAttr = {
     return (ds.base || 0) + (ds.plus || 0) + ((ds.base || 0) * (ds.pct || 0) / 100)
   },
 
-  getAttr ({ originalAttr, attr, weapon, char, game = 'gs' }) {
+  getAttr ({ originalAttr, attr = {}, weapon = {}, char = {}, game = 'gs' }) {
     let ret = {}
     if (originalAttr) {
       ret = lodash.merge({}, originalAttr)
     }
 
-    // 基础属性
     lodash.forEach('atk,def,hp,speed'.split(','), (key) => {
       ret[key] = AttrItem.create(originalAttr?.[key] || {
-        base: attr[`${key}Base`] * 1 || 0,
-        plus: attr[key] * 1 - attr[`${key}Base`] * 1 || 0,
+        base: (attr?.[`${key}Base`] || 0) * 1,
+        plus: (attr?.[key] || 0) * 1 - (attr?.[`${key}Base`] || 0) * 1,
         pct: 0
       })
     })
 
     lodash.forEach((game === 'gs' ? 'mastery,recharge,cpct,cdmg,heal,dmg,phy,coloringDmg' : 'recharge,cpct,cdmg,heal,dmg,enemydmg,effPct,effDef,stance,joy').split(','), (key) => {
       ret[key] = AttrItem.create(originalAttr?.[key] || {
-        base: attr[key] * 1 || 0,
+        base: (attr?.[key] || 0) * 1,
         plus: 0,
         pct: 0,
         inc: 0
       })
     })
 
-    // 技能属性记录
     lodash.forEach((game === 'gs' ? 'a,a2,a3,e,q,nightsoul' : 'a,a2,a3,e,e2,xe,q,q2,t,me,me2,mt,mt2,dot,break').split(','), (key) => {
       ret[key] = ret[key] || {
-        pct: 0, // 倍率加成
-        multi: 0, // 独立倍率乘区加成，宵宫E等
-
-        plus: 0, // 伤害值提高
-        dmg: 0, // 伤害提高
-        enemydmg: 0, // 承受伤害提高
-        cpct: 0, // 暴击提高
-        cdmg: 0, // 爆伤提高
-
-        elevated: 0, // 擢升
-
-        def: 0, // 防御降低
-        ignore: 0 // 无视防御
+        pct: 0,
+        multi: 0,
+        plus: 0,
+        dmg: 0,
+        enemydmg: 0,
+        cpct: 0,
+        cdmg: 0,
+        elevated: 0,
+        def: 0,
+        ignore: 0
       }
     })
 
     ret.enemy = ret.enemy || { def: 0, ignore: 0, phy: 0 }
-
     ret.shield = AttrItem.create(originalAttr?.shield || { base: 100, plus: 0, inc: 100 })
 
     if (!originalAttr) {
@@ -66,36 +57,35 @@ let DmgAttr = {
       ret.refine = ((weapon.affix || ret.refine || 1) * 1 - 1) || 0
       ret.multi = 0
       ret.kx = 0
-      ret.staticAttr = attr.staticAttr
+      ret.staticAttr = attr?.staticAttr || {}
       if (game === 'gs') {
-        ret.elevated = 0 // 擢升
-        ret.vaporize = 0 // 蒸发
-        ret.melt = 0 // 融化
-        ret.burning = 0 // 燃烧
-        ret.crystallize = 0 // 结晶
-        ret.superConduct = 0 // 超导
-        ret.swirl = 0 // 扩散
-        ret.electroCharged = 0 // 感电
-        ret.shatter = 0 // 碎冰
-        ret.overloaded = 0 // 超载
-        ret.bloom = 0 // 绽放
-        ret.burgeon = 0 // 烈绽放
-        ret.hyperBloom = 0 // 超绽放
-        ret.aggravate = 0 // 超激化
-        ret.spread = 0 // 蔓激化
-        ret.lunarCharged = 0 // 月感电
-        ret.lunarBloom = 0 // 月绽放
-        ret.lunarCrystallize = 0 // 月结晶
-        ret.fykx = 0 // 敌人反应抗性降低
-        ret.fyinc = 0 // 反应伤害值提升（百分比/不受精通加成）
-        ret.fyplus = 0 // 反应伤害值提升（数值/不受精通加成）
-        ret.fypct = 0 // 反应基础伤害值提升（百分比/受精通加成）
-        ret.fybase = 0 // 反应基础伤害值提升（数值/受精通加成）
+        ret.elevated = 0
+        ret.vaporize = 0
+        ret.melt = 0
+        ret.burning = 0
+        ret.crystallize = 0
+        ret.superConduct = 0
+        ret.swirl = 0
+        ret.electroCharged = 0
+        ret.shatter = 0
+        ret.overloaded = 0
+        ret.bloom = 0
+        ret.burgeon = 0
+        ret.hyperBloom = 0
+        ret.aggravate = 0
+        ret.spread = 0
+        ret.lunarCharged = 0
+        ret.lunarBloom = 0
+        ret.lunarCrystallize = 0
+        ret.fykx = 0
+        ret.fyinc = 0
+        ret.fyplus = 0
+        ret.fypct = 0
+        ret.fybase = 0
       } else if (game === 'sr') {
-        ret.sp = char.sp * 1
-        // 超击破
+        ret.sp = (char.sp || 0) * 1
         ret.superBreak = {
-          ignore: 0 // 无视防御
+          ignore: 0
         }
         ret.joy = 0
       }
@@ -156,14 +146,14 @@ let DmgAttr = {
       if (buff.mastery) {
         let mKey = { vaporize: '蒸发', melt: '融化', swirl: '扩散' }
         let mKey2 = { aggravate: '超激化', spread: '蔓激化' }
-        let mastery = Math.max(0, attr.mastery.base + attr.mastery.plus)
+        let mastery = Math.max(0, (attr.mastery?.base || 0) + (attr.mastery?.plus || 0))
         buff.data = buff.data || {}
         let key = buff.mastery
         if (mKey[key]) {
           buff.data['_' + key] = DmgMastery.getMultiple(key, mastery) * 100
         } else if (mKey2[key]) {
           let eleNum = DmgMastery.getBasePct(key, attr.element)
-          let eleBase = 1 + attr[key] / 100 + DmgMastery.getMultiple(key, mastery)
+          let eleBase = 1 + (attr[key] || 0) / 100 + DmgMastery.getMultiple(key, mastery)
           eleBase *= eleBaseDmg[ds.level]
           buff.data['_' + key] = DmgMastery.getMultiple(key, mastery) * 100
           buff.data['_' + key + 'num'] = eleNum * eleBase
@@ -175,23 +165,32 @@ let DmgAttr = {
         if (!val && val !== 0) return
         title = title.replace(`[${key}]`, Format.comma(val, 1))
 
-        // 技能提高
         let tRet = /^(a|a2|a3|e|q|t|me|xe|mt|dot|break|nightsoul)(Def|Ignore|Dmg|Enemydmg|Plus|Pct|Cpct|Cdmg|Multi|Elevated)$/.exec(key)
         if (tRet) {
-          attr[tRet[1]][tRet[2].toLowerCase()] += val * 1 || 0
+          if (attr[tRet[1]]) {
+            attr[tRet[1]][tRet[2].toLowerCase()] += val * 1 || 0
+          }
           return
         }
 
         let aRet = /^(mastery|cpct|cdmg|heal|recharge|dmg|enemydmg|phy|coloringDmg|shield|speed|stance|joy)(Plus|Pct|Inc)?$/.exec(key)
         if (aRet) {
-          attr[aRet[1]][aRet[2] ? aRet[2].toLowerCase() : 'plus'] += val * 1 || 0
+          if (attr[aRet[1]]) {
+            attr[aRet[1]][aRet[2] ? aRet[2].toLowerCase() : 'plus'] += val * 1 || 0
+          }
           return
         }
 
         let bRet = /^(hp|def|atk)(Base|Plus|Pct|Inc)?$/.exec(key)
         if (bRet) {
-          attr[bRet[1]][bRet[2] ? bRet[2].toLowerCase() : 'plus'] += val * 1 || 0
-          if (bRet[2] === 'Base') attr[bRet[1]].plus += val * attr.staticAttr[bRet[1]].pct / 100 || 0
+          let bKey = bRet[1]
+          let bType = bRet[2] ? bRet[2].toLowerCase() : 'plus'
+          if (attr[bKey]) {
+            attr[bKey][bType] += val * 1 || 0
+            if (bRet[2] === 'Base' && attr.staticAttr?.[bKey]) {
+              attr[bKey].plus += val * attr.staticAttr[bKey].pct / 100 || 0
+            }
+          }
           return
         }
 
@@ -205,7 +204,9 @@ let DmgAttr = {
 
         let sRet = /^(superBreak)(Ignore)$/.exec(key)
         if (sRet) {
-          attr[sRet[1]][sRet[2].toLowerCase()] += val * 1 || 0
+          if (attr[sRet[1]]) {
+            attr[sRet[1]][sRet[2].toLowerCase()] += val * 1 || 0
+          }
         }
       })
       msg.push(title)
